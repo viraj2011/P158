7d3f7a5682b8094475230381e9f7607799993d8a
viraj2011 <107635321+viraj2011@users.noreply.github.com>
2024-01-19 21:52:23 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,34 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleMouseEnterEvents();
+    
+  },
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#D76B30",
+        opacity: 1,
+      });
+    }
+  },
+  handleMouseEnterEvents: function () {
+    // Mouse Enter Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    // Mouse Leave Events
+    
+  },
+});
